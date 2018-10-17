920e596a0f48d89a7cb0ec7a891869b456c90f7f
boubinjg <boubinjg@gmail.com>
2018-10-17 16:24:38 -0400

    709

---
+++ 709.cpp
@@ -0,0 +1,9 @@
+#include<algorithm>
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        return str;
+    }
+};
